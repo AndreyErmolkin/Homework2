fc8d627cc0c752ca6aa26caa50a8036e47be91f0
AndreyErmolkin <78263246+AndreyErmolkin@users.noreply.github.com>
2021-02-21 16:52:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HomeWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                byte a;
+                a = 25;
+                int b = a;
+                short d;
+                d = 2500;
+                double c = d;
+                uint r;
+                r = 1500456U;
+                long t = r;
+                {
+                    float i = 56.56F;
+                    double o = (double)i;
+                    double p = 9876433;
+                    decimal l = (decimal)p;
+                    ulong h = 45UL;
+                    double e = (double)h;
+                }
+
+                Console.WriteLine("Преобразования верны");
+            }
+        }
+
+
+    }
+}
